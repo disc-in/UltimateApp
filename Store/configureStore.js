bc748bf8c6d23ca57bb3278658e3f53057929a84
Romain <aenario@gmail.com>
2020-03-20 19:35:39 +0100

    Move animations to fixtures

--- Store/configureStore.js
+++ Store/configureStore.js
@@ -1,67 +1,10 @@
 // Store/configureStore.js
 
 import { createStore } from 'redux';
-import toggleFavorite from './Reducers/favoriteReducer'
+import fixtures from './Fixtures/index'
+
+console.log('THERE', fixtures)
 
 const noreducer = (x) => x
 
-export default createStore(noreducer, {
-    drills: [{
-        id: 1,
-        type: 'fitness', // vs technical vs collectif
-        source: "Rise Up",
-        title: "Hot Box",
-        img: "https://gazettesports.fr/wp-content/uploads/2018/11/ultimate_frisbee_gloves.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque dignissim felis, at tristique mi efficitur a. Nulla pellentesque odio nunc, ut sodales ex pulvinar at. In hac habitasse platea dictumst. Pellentesque sodales nisl lorem, ac lacinia nisl lacinia eu. Proin lacinia viverra mauris, et pharetra ipsum tempus eget. Ut maximus sapien in hendrerit ultricies. Maecenas vel diam tincidunt, consequat nunc a, mattis eros. Curabitur a eros a nulla malesuada gravida. Donec massa quam, suscipit eget efficitur et, euismod sed nulla. Morbi mattis et magna a aliquam. Fusce pellentesque vel erat eget",
-        nbPlayers: 2,
-        duration: 900, // seconds
-        goals: ['Catch', 'Handling'],
-        level: ['intermediate'],
-        steps: [
-            {
-                title: "Text",
-                subtitle: "Text",
-                animation: { /* format à déterminer */ }
-            },
-            {
-                title: "Text",
-                subtitle: "Text",
-                videoYoutube: "url"
-            },
-            {
-                title: "Text",
-                subtitle: "Text",
-                webview: "url"
-            },
-        ]
-
-    }, {
-        id: 1,
-        type: 'tactics', // vs technical vs collectif
-        source: "Flikulti",
-        title: "Defense Positioning",
-        img: "https://gazettesports.fr/wp-content/uploads/2018/11/ultimate_frisbee_gloves.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque dignissim felis, at tristique mi efficitur a. Nulla pellentesque odio nunc, ut sodales ex pulvinar at. In hac habitasse platea dictumst. Pellentesque sodales nisl lorem, ac lacinia nisl lacinia eu. Proin lacinia viverra mauris, et pharetra ipsum tempus eget. Ut maximus sapien in hendrerit ultricies. Maecenas vel diam tincidunt, consequat nunc a, mattis eros. Curabitur a eros a nulla malesuada gravida. Donec massa quam, suscipit eget efficitur et, euismod sed nulla. Morbi mattis et magna a aliquam. Fusce pellentesque vel erat eget",
-        nbPlayers: 2,
-        duration: 1200, // seconds
-        goals: ['Catch', 'Handling'],
-        level: ['intermediate'],
-        steps: [
-            {
-                title: "Text",
-                subtitle: "Text",
-                animation: { /* format à déterminer */ }
-            },
-            {
-                title: "Text",
-                subtitle: "Text",
-                videoYoutube: "url"
-            },
-            {
-                title: "Text",
-                subtitle: "Text",
-                webview: "url"
-            },
-        ]
-    }]
-})
+export default createStore(noreducer, fixtures)

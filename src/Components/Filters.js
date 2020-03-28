95de818faefd4309f9078ac06998ed6b94d32b6d
Martin Salles <salles.martin@gmail.com>
2020-03-28 13:14:07 +0100

    Add goals to filters (duplication)

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -11,25 +11,52 @@ const Button = props => {
 };
 
 const Filters = props => {
-  const [level, setLevel] = useState();
+  const [selectedLevel, setLevel] = useState();
+  const [selectedGoal, setGoal] = useState();
 
   const onLevelChange = pressedLevel => {
-    const newLevel = pressedLevel === level ? undefined : pressedLevel;
+    const newLevel = pressedLevel === selectedLevel ? undefined : pressedLevel;
     setLevel(newLevel);
+  };
+  const onGoalChange = pressedGoal => {
+    const newGoal = pressedGoal === selectedGoal ? undefined : pressedGoal;
+    setGoal(newGoal);
   };
 
   useEffect(() => {
-    const newData = level ? props.initialData.filter(drill => drill.level === level) : props.initialData;
+    const newData = selectedLevel
+      ? props.initialData.filter(drill => drill.level === selectedLevel)
+      : props.initialData;
     props.onFiltered(newData);
-  }, [level]);
+  }, [selectedLevel]);
+
+  useEffect(() => {
+    const newData = selectedGoal
+      ? props.initialData.filter(drill => drill.goals.includes(selectedGoal))
+      : props.initialData;
+    props.onFiltered(newData);
+  }, [selectedGoal]);
 
   return (
     <View style={styles.filters}>
       <Text style={styles.filterTitle}>Level</Text>
       <View style={styles.filter}>
-        <Button title="Beginner" onPress={() => onLevelChange('beginner')} active={level === 'beginner'} />
-        <Button title="Intermediate" onPress={() => onLevelChange('intermediate')} active={level === 'intermediate'} />
-        <Button title="Advanced" onPress={() => onLevelChange('advanced')} active={level === 'advanced'} />
+        <Button title="Beginner" onPress={() => onLevelChange('beginner')} active={selectedLevel === 'beginner'} />
+        <Button
+          title="Intermediate"
+          onPress={() => onLevelChange('intermediate')}
+          active={selectedLevel === 'intermediate'}
+        />
+        <Button title="Advanced" onPress={() => onLevelChange('advanced')} active={selectedLevel === 'advanced'} />
+      </View>
+      <Text style={styles.filterTitle}>Goals</Text>
+      <View style={styles.filter}>
+        {props.initialData
+          .flatMap(drill => drill.goals)
+          .filter((goal, index, array) => array.indexOf(goal) === index)
+          .map(goal => (
+            <Button key={goal} title={goal} onPress={() => onGoalChange(goal)} active={selectedGoal === goal} />
+          ))}
       </View>
       <Button title="Validate" onPress={props.onConfirm} />
     </View>
@@ -42,12 +69,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   filterTitle: {
-    marginTop: 20,
+    marginTop: 10,
     textTransform: 'uppercase',
     fontWeight: 'bold',
   },
   filter: {
+    marginBottom: 10,
     flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
   },
   button: {
     borderColor: '#e5e5e5',

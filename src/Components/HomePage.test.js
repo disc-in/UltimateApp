7ea499e971c4ff45f5b4680be8fc343bb7002116
Martin Salles <salles.martin@gmail.com>
2020-04-01 00:28:42 +0200

    Add react-native-testing-library component test

--- src/Components/HomePage.test.js
+++ src/Components/HomePage.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import { render, fireEvent, waitForElement } from 'react-native-testing-library';
 import HomePage from './HomePage';
 
 describe('<HomePage />', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<HomePage />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('links to technical drill list', async () => {
+    const navigation = { navigate: jest.fn() };
+    const { container, getByText, debug } = render(<HomePage navigation={navigation} />);
+
+    await fireEvent.press(getByText('Frisbee'));
+
+    expect(navigation.navigate).toBeCalledWith('DrillListPage', { type: 'technical' });
   });
 });

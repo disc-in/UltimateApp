bb0caea2e5186ea451c8f2db1f10977d38064738
Martin Salles <salles.martin@gmail.com>
2020-04-04 18:21:02 +0200

    Add Filters tests

--- src/Components/Filters.test.js
+++ src/Components/Filters.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from 'react-native-testing-library';
 
 import Filters from './Filters';
 
@@ -7,5 +8,24 @@ describe('<Filters />', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Filters initialData={[]} onConfirm={jest.fn()} onFiltered={jest.fn()} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('filtering', () => {
+    const onConfirm = jest.fn();
+    const onFiltered = jest.fn();
+
+    it('filters drills by level', async () => {
+      const { container, getByText } = render(
+        <Filters initialData={[]} onConfirm={onConfirm} onFiltered={onFiltered} />,
+      );
+
+      await fireEvent.press(getByText('Beginner'));
+
+      expect(onFiltered).toBeCalledWith([]);
+
+      await fireEvent.press(getByText('Validate'));
+
+      expect(onConfirm).toBeCalled();
+    });
   });
 });

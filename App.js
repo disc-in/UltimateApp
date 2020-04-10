8e5a202b75833848a68524bb5c4229d8277f7413
Martin Salles <salles.martin@gmail.com>
2020-04-11 00:35:46 +0200

    Enable react-native-screens for faster navigation

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import 'react-native-gesture-handler';
-
 import React from 'react';
-import { Navigation } from './src/Navigation';
+import { enableScreens } from 'react-native-screens';
 import { Provider } from 'react-redux';
+import { Navigation } from './src/Navigation';
 import Store from './src/Store/configureStore';
+
+enableScreens();
 
 export default class App extends React.Component {
   render() {

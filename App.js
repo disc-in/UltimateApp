944864b9699082da8015fae9bfad10ceafa47574
Martin Salles <salles.martin@gmail.com>
2020-04-13 14:47:05 +0200

    Optimize app loading (#69)
    * Enable react-native-screens for faster navigation
    
    * Reopen app where it was previously
    
    * Remove useless import

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
 import 'react-native-gesture-handler';
-
 import React from 'react';
-import { Navigation } from './src/Navigation';
+import { AsyncStorage } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
 import { Provider } from 'react-redux';
+import { Navigation } from './src/Navigation';
 import Store from './src/Store/configureStore';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={Store}>
-        <Navigation />
-      </Provider>
-    );
+enableScreens();
+
+const PERSISTENCE_KEY = 'NAVIGATION_STATE';
+
+const App = props => {
+  const [isReady, setIsReady] = React.useState(false);
+  const [initialState, setInitialState] = React.useState();
+
+  React.useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+        const state = JSON.parse(savedStateString);
+
+        setInitialState(state);
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
   }
-}
+
+  return (
+    <Provider store={Store}>
+      <NavigationContainer
+        initialState={initialState}
+        onStateChange={state => AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))}
+      >
+        <Navigation />
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;

e5665a9419ae0c65519977151dc1826f75692560
arribbar <barthelemy.arribe@mediapps.io>
2020-04-13 21:10:07 +0200

    Update one test in Drill page

--- src/Components/DrillPage.test.js
+++ src/Components/DrillPage.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import renderer, { TestRenderer } from 'react-test-renderer';
+import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { render, fireEvent } from 'react-native-testing-library';
+
 import store from '../Store/testStore';
 import { createDrill } from '../Fixtures/TestFixtures';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 
 import ConnectedDrillPage, { DrillPage } from './DrillPage';
 import * as Actions from '../Store/Actions/favoriteAction';
+import { useHeaderHeight } from '@react-navigation/stack';
+jest.mock('@react-navigation/stack');
+const { createStackNavigator } = jest.requireActual('');
 
 describe('<DrillPage />', () => {
-  const drill = createDrill({ title: '________' });
+  const drill = createDrill();
   const route = {
     params: {
       drill,
@@ -19,43 +22,58 @@ describe('<DrillPage />', () => {
   };
 
   it('renders correctly', () => {
-    const Stack = createStackNavigator();
+    useHeaderHeight.mockReturnValue(80);
+    const navigation = { setOptions: jest.fn(), navigate: jest.fn() };
+
     const tree = renderer
       .create(
         <Provider store={store}>
-          <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen name="DrillPage" component={ConnectedDrillPage} initialParams={{ drill }} />
-            </Stack.Navigator>
-          </NavigationContainer>
+          <DrillPage route={route} navigation={navigation} />
         </Provider>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  // it('toggles favorite drill', async () => {
-  //   const Stack = createStackNavigator();
+  /* it('toggles favorite drill', async () => {
+    // useHeaderHeight.mockReturnValue(80);
+    // const navigation = { setOptions: jest.fn(), navigate: jest.fn() };
+    const toggleFavorite = jest.fn();
+    const DummyScreen = props => null;
 
-  //   const { getByTestId } = render(
-  //     <Provider store={store}>
-  //       <NavigationContainer>
-  //         <Stack.Navigator>
-  //           <Stack.Screen name="DrillPage" component={ConnectedDrillPage} initialParams={{ drill }} />
-  //         </Stack.Navigator>
-  //       </NavigationContainer>
-  //     </Provider>,
-  //   );
-  //   const spy = jest.spyOn(Actions, 'toggleFavorite');
 
-  //   await fireEvent.press(getByTestId('favoriteButton'));
-  //   expect(spy).toHaveBeenCalled();
-  //   expect(Actions.toggleFavorite).toBeCalledWith(
-  //     expect.objectContaining({
-  //       id: 1,
-  //     }),
-  //   );
+    // it('renders correctly', () => {
+    //   const Stack = createStackNavigator();
+    //   const tree = renderer
+    //     .create(
+    //       <Provider store={store}>
+    //         <NavigationContainer>
+    //           <Stack.Navigator>
+    //             <Stack.Screen name="DrillPage" component={ConnectedDrillPage} initialParams={{ drill }} />
+    //           </Stack.Navigator>
+    //         </NavigationContainer>
+    //       </Provider>,
+    //     )
+    //     .toJSON();
+    //   expect(tree).toMatchSnapshot();
+    // });
+    // const { getByTestId } = render(
+    //   <DrillPage route={route} navigation={navigation} favoriteDrills={[]} toggleFavorite={toggleFavorite} />,
+    // );
 
-  //   spy.mockRestore();
-  // });
+
+    const Stack = createStackNavigator();
+    const { getByText, getByTestId } = render(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="DrillPage" component={DrillPage} initialParams={drill} />
+          <Stack.Screen name="DrillListPage" component={DummyScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>,
+    );
+
+    await fireEvent.press(getByTestId('favoriteButton'));
+
+    expect(toggleFavorite).toBeCalledWith(drill);
+  }); */
 });

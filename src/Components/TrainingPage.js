9f01542854925efad60bc0fa1e5f69b88cd55b67
Martin Salles <salles.martin@gmail.com>
2020-04-18 17:44:19 +0200

    Add real drills and trainings (#74)
    * Add 2 trainings
    
    * Use test store in test
    
    * Use scrollview to display the whole content
    
    * Add duration and tweak training page
    
    * Add third training
    
    * Add test

--- src/Components/TrainingPage.js
+++ src/Components/TrainingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import DrillList from './shared/DrillList';
 import theme from '../styles/theme.style';
@@ -9,8 +9,7 @@ export function getGoals(trainingDrills) {
 }
 
 export function getTrainingDuration(trainingDrills) {
-  const durationList = trainingDrills.map(({ durationInMinutes }) => durationInMinutes);
-  return durationList.reduce((a, b) => a + b, 0);
+  return trainingDrills.reduce((total, drill) => total + drill.durationInMinutes, 0);
 }
 
 export function getTrainingMinimalPlayersNumber(trainingDrills) {
@@ -18,65 +17,74 @@ export function getTrainingMinimalPlayersNumber(trainingDrills) {
   return Math.max(...minimalPlayersNumberList);
 }
 
+export const TrainingPage = props => {
+  const { navigation, route, drills } = props;
+  const training = route.params.training;
+  const trainingDrills = training.drills.map(drillId => drills.find(drill => drill.id === drillId));
+
+  return (
+    <ScrollView style={styles.trainingPage} contentContainerStyle={styles.trainingPageContent}>
+      <View style={styles.infos}>
+        <Text style={styles.descriptionText}>{training.description}</Text>
+        <View style={styles.infoDisplay}>
+          <Text style={styles.infoTitle}>Players</Text>
+          <Text style={styles.info}>{getTrainingMinimalPlayersNumber(trainingDrills)}+</Text>
+        </View>
+        <View style={styles.infoDisplay}>
+          <Text style={styles.infoTitle}>Duration</Text>
+          <Text style={styles.info}>{getTrainingDuration(trainingDrills)}+</Text>
+        </View>
+        <View style={styles.infoDisplay}>
+          <Text style={styles.infoTitle}>Goals</Text>
+          <Text style={styles.info}>{getGoals(trainingDrills).join(', ')}</Text>
+        </View>
+      </View>
+      <DrillList navigation={navigation} drillsToDisplay={trainingDrills} />
+    </ScrollView>
+  );
+};
+
 const mapStateToProps = state => {
   return {
     drills: state.drills,
   };
 };
 
-export class TrainingPage extends Component {
-  render() {
-    const { navigation } = this.props;
-    const training = this.props.route.params.training;
-    const drills = training.drills.map(drillId => this.props.drills.find(drill => drill.id === drillId));
-
-    return (
-      <View style={styles.trainingPage}>
-        <Text style={styles.descriptionText}>{training.description}</Text>
-        <View style={styles.infoDisplay}>
-          <Text style={styles.infoTitle}>Players:</Text>
-          <Text style={styles.info}>{getTrainingMinimalPlayersNumber(drills)}+</Text>
-        </View>
-        <View style={styles.infoDisplay}>
-          <Text style={styles.infoTitle}>Goals:</Text>
-          <Text style={styles.info}>{getGoals(drills).join(', ')}</Text>
-        </View>
-        <DrillList navigation={navigation} drillsToDisplay={drills} />
-      </View>
-    );
-  }
-}
-
 export default connect(mapStateToProps)(TrainingPage);
 
 const styles = StyleSheet.create({
   trainingPage: {
-    paddingTop: 10,
     paddingLeft: 20,
     backgroundColor: theme.BACKGROUND_COLOR_LIGHT,
     height: '100%',
   },
+  trainingPageContent: {
+    paddingVertical: 20,
+  },
+  infos: {
+    marginBottom: 20,
+  },
   descriptionText: {
-    padding: 20,
     color: theme.COLOR_SECONDARY,
-    textAlign: 'center',
     fontSize: theme.FONT_SIZE_MEDIUM,
   },
   infoDisplay: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    padding: 10,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingVertical: 10,
   },
   infoTitle: {
     color: theme.COLOR_PRIMARY,
-    paddingHorizontal: 30,
     fontSize: theme.FONT_SIZE_MEDIUM,
     fontWeight: 'bold',
+    flexBasis: 1,
+    flexGrow: 1,
   },
   info: {
     color: theme.COLOR_SECONDARY,
-    paddingHorizontal: 30,
     fontSize: theme.FONT_SIZE_MEDIUM,
+    flexBasis: 4,
+    flexGrow: 4,
   },
 });
---
+++ src/Components/shared/DrillList.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent, cleanup } from 'react-native-testing-library';
+import { createDrill } from '../../Fixtures/TestFixtures';
+
+import DrillList from './DrillList';
+
+afterEach(cleanup);
+
+describe('<DrillList />', () => {
+  it('renders correctly', () => {
+    const drills = [createDrill()];
+    const tree = renderer.create(<DrillList drillsToDisplay={drills} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});

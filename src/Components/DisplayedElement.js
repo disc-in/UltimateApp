d3f6c4d6b444958acabc17d54a585c34b732c91f
Romain <aenario@gmail.com>
2020-04-23 19:43:27 +0200

    eslint over animate

--- src/Components/DisplayedElement.js
+++ src/Components/DisplayedElement.js
@@ -3,8 +3,7 @@ import { StyleSheet, Easing, Animated, View, PanResponder } from 'react-native';
 
 /** An element displayed in a drill animation */
 class DisplayedElement extends React.Component {
-
-    /* Props must contain:
+  /* Props must contain:
       - id: which indicates how to display the element
       ("offense", "defense", "triangle" or "disc") 
 
@@ -15,220 +14,204 @@ class DisplayedElement extends React.Component {
       - eId: element index in the list of elements of the drill (-1 if it is not currently in a drill)
       
     */
-    constructor(props) {
-	super(props);
+  constructor(props) {
+    super(props);
 
-        // TODO: put the constant coefficient used in the following somewhere to avoir writing them twice (in this class and in DrillCuts)
-	var dimensionMin = Math.min(this.props.animationWidth, this.props.animationHeight);
-	this.props.playerRadius = dimensionMin / 12;
-        this.props.discRadius = 200; 
-	this.props.coneSize = this.props.playerRadius * 5 / 16;
+    // TODO: put the constant coefficient used in the following somewhere to avoir writing them twice (in this class and in DrillCuts)
+    var dimensionMin = Math.min(this.props.animationWidth, this.props.animationHeight);
+    this.props.playerRadius = dimensionMin / 12;
+    this.props.discRadius = 200;
+    this.props.coneSize = (this.props.playerRadius * 5) / 16;
 
-	this.props.bottomconeSize = this.props.playerRadius * 10 / 16;
-        this.props.borderWidth = this.props.discRadius/10;
-	
-	/* Current position of the element in pixels */ 
-	this.currentPosition = new Animated.ValueXY({ x: 0, y: 0 });
-        
-        this.xCut = 10;
-        this.yCut = 10;
+    this.props.bottomconeSize = (this.props.playerRadius * 10) / 16;
+    this.props.borderWidth = this.props.discRadius / 10;
 
-	this.top = 0;
-	this.left = 0;
-	
-	// Add a listener on each coordinate offset to get its value at the end of each move
-	this.currentPosition.x.addListener(({value}) => this._value = value);
-	this.currentPosition.y.addListener(({value}) => this._value = value);
+    /* Current position of the element in pixels */
 
-	
-        // True if the element has already been moved
-        this.moved = false;
-        this._val = { x:0, y:0 };
-        this.previousX = -1;
-        this.previousY = -1;
-        
-        this.currentPosition.addListener((value) => this._val = value); // Initialize PanResponder with move handling
+    this.currentPosition = new Animated.ValueXY({ x: 0, y: 0 });
 
-        // Initiate the panResponder
-        this.panResponder = PanResponder.create({
+    this.xCut = 10;
+    this.yCut = 10;
 
-	    // Ask to be the responder
-	    onStartShouldSetPanResponder: (e, gesture) => true,
+    this.top = 0;
+    this.left = 0;
 
-	    // Called when the gesture starts
-	    onPanResponderGrant: (e, gesture) => {
+    // Add a listener on each coordinate offset to get its value at the end of each move
+    this.currentPosition.x.addListener(({ value }) => (this._value = value));
+    this.currentPosition.y.addListener(({ value }) => (this._value = value));
 
-                // We always want an element displayed at the original position of the first element.
-                // If the current element A is moved for the first time, we create a new element B at the original position of A
-                // If A is moved again, we do not do anything. B (or an element created by B) will be at the original position of A.
-                if(this.props.movable && this.props.onClick != undefined && !this.moved){
-                    
-		    this.currentPosition.setOffset({
-			x: this._val.x,
-			y: this._val.y
-		    });
-                    
-		    this.props.onClick();
-		    this.moved = true;
-		     
-		    this.currentPosition.setValue({ x:0, y:0});
-                }
-	    },
+    // True if the element has already been moved
+    this.moved = false;
+    this._val = { x: 0, y: 0 };
+    this.previousX = -1;
+    this.previousY = -1;
 
-	    // Called when a move is made
-	    onPanResponderMove: this.props.movable?Animated.event([
-                null, { dx: this.currentPosition.x, dy: this.currentPosition.y }
-	    ]):undefined,
+    this.currentPosition.addListener(value => (this._val = value)); // Initialize PanResponder with move handling
 
-	    onPanResponderRelease: (evt, gesturestate) => {
+    // Initiate the panResponder
+    this.panResponder = PanResponder.create({
+      // Ask to be the responder
+      onStartShouldSetPanResponder: (e, gesture) => true,
 
-                
-		if(this.props.movable && this.props.onMoveEnd != undefined){
-                    this.props.onMoveEnd(this, this.currentPosition.x._value, this.currentPosition.y._value);
-		    this.currentPosition.setValue({ x:0, y:0});
-                }
-                
-                
-	    }
-        });
+      // Called when the gesture starts
+      onPanResponderGrant: (e, gesture) => {
+        // We always want an element displayed at the original position of the first element.
+        // If the current element A is moved for the first time, we create a new element B at the original position of A
+        // If A is moved again, we do not do anything. B (or an element created by B) will be at the original position of A.
+        if (this.props.movable && this.props.onClick != undefined && !this.moved) {
+          this.currentPosition.setOffset({
+            x: this._val.x,
+            y: this._val.y,
+          });
+
+          this.props.onClick();
+          this.moved = true;
+
+          this.currentPosition.setValue({ x: 0, y: 0 });
+        }
+      },
+
+      // Called when a move is made
+      onPanResponderMove: this.props.movable
+        ? Animated.event([null, { dx: this.currentPosition.x, dy: this.currentPosition.y }])
+        : undefined,
+
+      onPanResponderRelease: (evt, gesturestate) => {
+        if (this.props.movable && this.props.onMoveEnd != undefined) {
+          this.props.onMoveEnd(this, this.currentPosition.x._value, this.currentPosition.y._value);
+          this.currentPosition.setValue({ x: 0, y: 0 });
+        }
+      },
+    });
+  }
+
+  /** Set the position of the element (the argument are in pixels not in percentage of the screen) */
+  setPosition(xArg, yArg) {
+    this.currentPosition.setValue({ x: xArg, y: yArg });
+  }
+
+  /** Get an animation to move the element at a given position */
+  getAnimation(xValue, yValue, durationValue) {
+    return Animated.timing(this.currentPosition, {
+      toValue: { x: xValue, y: yValue },
+      duration: durationValue,
+      easing: Easing.linear,
+    });
+  }
+
+  render() {
+    const panStyle = {
+      transform: this.currentPosition.getTranslateTransform(),
+    };
+
+    /* Returns a component according to the element type */
+    switch (this.props.id) {
+      case 'defense':
+        return (
+          <Animated.Text
+            // Use the panResponder in this view
+            {...this.panResponder.panHandlers}
+            style={[panStyle, styles.defense, { height: 40 }, { width: 40 }, { borderRadius: 40 }]}
+            key={this.props.key}
+          >
+            {this.props.number}
+          </Animated.Text>
+        );
+
+      case 'offense':
+        return (
+          <Animated.Text
+            // Use the panResponder in this view
+            {...this.panResponder.panHandlers}
+            style={[
+              panStyle,
+              styles.offense,
+              { height: 40 },
+              { width: 40 },
+              { borderRadius: 40 },
+              { top: this.top },
+              { left: this.left },
+            ]}
+            key={this.props.key}
+          >
+            {this.props.number}
+          </Animated.Text>
+        );
+
+      case 'disc':
+        return (
+          <Animated.View
+            // Use the panResponder in this view
+            {...this.panResponder.panHandlers}
+            style={[
+              panStyle,
+              styles.disc,
+              { height: 20 },
+              { width: 20 },
+              { borderRadius: 20 },
+              { borderWidth: 2 },
+              { top: this.top },
+              { left: this.left },
+            ]}
+            key={this.props.key}
+          />
+        );
+
+      case 'triangle':
+        return (
+          <Animated.View
+            // Use the panResponder in this view
+            {...this.panResponder.panHandlers}
+            style={[
+              panStyle,
+              styles.triangle,
+              { borderLeftWidth: 12 },
+              { borderRightWidth: 12 },
+              { borderBottomWidth: 25 },
+              { top: this.top },
+              { left: this.left },
+            ]}
+            key={this.props.key}
+          />
+        );
+
+      default:
+        return <View key={this.props.key} />;
     }
-
-    /** Set the position of the element (the argument are in pixels not in percentage of the screen) */
-    setPosition(xArg, yArg) {
-	this.currentPosition.setValue({ x: xArg, y: yArg});
-    }
-
-    /** Get an animation to move the element at a given position */
-    getAnimation(xValue, yValue, durationValue) {
-	return Animated.timing(this.currentPosition, {
-	    toValue: { x: xValue, y: yValue },
-	    duration: durationValue,
-	    easing: Easing.linear,
-	});
-    }
-
-    render() {
-        
-	const panStyle = {
-	    transform: this.currentPosition.getTranslateTransform()
-	};
-
-	/* Returns a component according to the element type */
-	switch (this.props.id) {
-	    
-	case 'defense':
-	    
-            return (
-		<Animated.Text
-
-                // Use the panResponder in this view
-                {...this.panResponder.panHandlers}
-
-		style={[panStyle,styles.defense,
-			{height: 40},
-			{width: 40},
-			{borderRadius: 40},]}
-		key={this.props.key}
-		    >
-		    {this.props.number}
-		</Animated.Text>
-            );
-
-	case 'offense':
-            return (
-		    <Animated.Text
-
-                // Use the panResponder in this view
-                {...this.panResponder.panHandlers}
-
-		style={[panStyle,styles.offense,
-			{height: 40},
-			{width: 40},
-			{borderRadius: 40},
-			{top: this.top},
-			{left: this.left}
-		       ]}
-		key={this.props.key}
-		    >
-		    {this.props.number}
-		</Animated.Text>
-            );
-
-	case 'disc':
-            return (
-
-		    <Animated.View
-
-                // Use the panResponder in this view
-                {...this.panResponder.panHandlers}
-
-		style={[panStyle,styles.disc, 
-			{height: 20},
-			{width: 20},
-			{borderRadius: 20},
-			{borderWidth: 2}, 
-			{top: this.top},
-			{left: this.left}]}
-		key={this.props.key}/>
-            );
-
-	case 'triangle':
-            return (
-		    <Animated.View
-
-                // Use the panResponder in this view
-                {...this.panResponder.panHandlers}
-
-		style={[panStyle,styles.triangle, 
-			{borderLeftWidth: 12},
-			{borderRightWidth: 12},
-			{borderBottomWidth: 25}, 
-			{top: this.top},
-			{left: this.left}
-		       ]}
-		key={this.props.key}
-		    />
-	    );
-
-	default:
-	    return <View key={this.props.key}/>;
-	}
-    }
+  }
 }
 
 const styles = StyleSheet.create({
+  defense: {
+    position: 'absolute',
+    backgroundColor: '#dcdcdc',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'black',
+  },
 
-    defense: {
-	position: 'absolute',
-	backgroundColor: '#dcdcdc',
-	textAlign: 'center',
-	textAlignVertical: 'center',
-	color: 'black',
-    },
-    
-    offense: {
-	position: 'absolute',
-	backgroundColor: '#cd5c5c',
-	textAlign: 'center',
-	textAlignVertical: 'center',
-	color: 'white',
-    },
+  offense: {
+    position: 'absolute',
+    backgroundColor: '#cd5c5c',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'white',
+  },
 
-    disc: {
-	position: 'absolute', 
-	borderColor: 'black',
-	backgroundColor: 'white',
-    },
+  disc: {
+    position: 'absolute',
+    borderColor: 'black',
+    backgroundColor: 'white',
+  },
 
-    triangle: {
-	position: 'absolute',
-	backgroundColor: 'transparent',
-	borderStyle: 'solid',
-	borderLeftColor: 'transparent',
-	borderRightColor: 'transparent',
-	borderBottomColor: 'orange',
-    },
+  triangle: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'orange',
+  },
 });
-
 
 export default DisplayedElement;
--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -6,173 +6,211 @@ import { Text, StyleSheet, Easing, Animated, View, TouchableOpacity } from 'reac
     - shows the current step;
     - enable to move to a given step by clicking on the associated button
  */
-class ProgressBar extends React.Component{
+class ProgressBar extends React.Component {
+  // The props contains:
+  // - animationWidth/animationHeight: size in pixel of the animation
+  // - stepCount: the number of steps
+  // - currentStepAV: an animated value which represents the current step
+  // - getStepAnimation(): set the animation to a given step
+  // - stepLength: length of an animation step in ms
+  constructor(props) {
+    super(props);
 
-    // The props contains:
-    // - animationWidth/animationHeight: size in pixel of the animation
-    // - stepCount: the number of steps
-    // - currentStepAV: an animated value which represents the current step
-    // - getStepAnimation(): set the animation to a given step
-    // - stepLength: length of an animation step in ms
-    constructor(props){
+    this.state = {
+      animationWidth: 1,
+      animationHeight: 1,
+    };
 
-	super(props);
+    // Opacity of the step dots (visible if the current step is >= to the step represented by the dot)
+    this.dotsOpacity = [];
 
-	this.state = {
-	    animationWidth: 1,
-	    animationHeight: 1
-	};
+    for (var i = 0; i < this.props.stepCount; i++)
+      // The first step is always visible
+      if (i == 0) this.dotsOpacity.push(new Animated.Value(1));
+      else this.dotsOpacity.push(new Animated.Value(0));
 
-	// Opacity of the step dots (visible if the current step is >= to the step represented by the dot)
-        this.dotsOpacity = [];
+    // Horizontal position of each dot
+    this.progressBarDots = [];
 
-        for(var i = 0; i < this.props.stepCount; i++)
+    // Width of the progress bar
+    this.progressBarWidth = this.props.animationWidth - 40;
+    var stepWidth = this.progressBarWidth / (this.props.stepCount - 1);
 
-	    // The first step is always visible
-            if(i == 0)
-                this.dotsOpacity.push(new Animated.Value(1));
-            else 
-                this.dotsOpacity.push(new Animated.Value(0));
+    //        if(this.props.animationWidth > 50)
+    // Interpolation of the current step which enables to get the current progress bar width
+    this.dynamiqueCurrentStep = this.props.currentStepAV.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, stepWidth],
+    });
 
-        // Horizontal position of each dot
-	this.progressBarDots = [];
-
-	// Width of the progress bar 
-	this.progressBarWidth = this.props.animationWidth-40;
-	var stepWidth = this.progressBarWidth / (this.props.stepCount - 1);
-	
-//        if(this.props.animationWidth > 50)
-    	// Interpolation of the current step which enables to get the current progress bar width
-	this.dynamiqueCurrentStep = this.props.currentStepAV.interpolate({
-	    inputRange: [0, 1],
-	    outputRange: [0, stepWidth]
-	});
-
-	// Set the horizontal position in pixels of each dot on the progress bar
-	for(i = 0; i < this.props.stepCount; i++){
-	    this.progressBarDots = this.appendObjTo(this.progressBarDots, {
-		key: i,
-		left: 10 + i * stepWidth
-	    });
-	}
-
-        // Contains the graphical components in the progress bar
-	this.progressBarComponents = [];
-        
-        /* Add the gray dots */
-	this.progressBarComponents = this.progressBarDots.map(item => <View style={[styles.dot, {left: item.left}, {top: this.props.animationHeight}]} key={item.key}/>);
-        
-        /* Add the gray bar */
-	this.progressBarComponents.push(<Animated.View style={[styles.progress_bar, {top: this.props.animationHeight + 6}, {width: this.progressBarWidth}, {left: 20}]} key={3*this.progressBarComponents.length+1}/>);
-
-        /* Add the blue dots */
-	this.progressBarComponents = this.progressBarComponents.concat(this.progressBarDots.map(item => <Animated.View
-												style={[styles.activated_dot, {opacity: this.dotsOpacity[item.key]}, {left: this.progressBarDots[item.key].left}, {top: this.props.animationHeight}]}
-												key={this.progressBarDots[item.key].key+this.progressBarDots.length}/>));
-
-        /* Add the objects used to detect the clicks on the dots */
-//        if(this.props.animationWidth > 50)
-	    this.progressBarComponents = this.progressBarComponents.concat(this.progressBarDots.map(item =>  <TouchableOpacity
-												    style={[{position: 'absolute'}, {height: 40}, {width: 40}, {left: this.progressBarDots[item.key].left-15}, {top: this.props.animationHeight-15}]}
-												    onPress={() => this._stepButtonClicked(item.key)}
-												    key={1000+item.key}/>));
-
-	
-        /* Add the step numbers */
-	this.progressBarComponents = this.progressBarComponents.concat(this.progressBarDots.map(item => <Text style={[{position: 'absolute'}, {left: item.left+4}, {top: this.props.animationHeight - 19}]} key={item.key+2*this.progressBarDots.length}>{item.key+1}</Text>));
-
-        /* Add the blue bar */
-	this.progressBarComponents.push(<Animated.View style={[styles.activated_progress_bar, {top: this.props.animationHeight + 6}, {width: this.dynamiqueCurrentStep}, {left: 20}]} key={3*this.progressBarComponents.length}/>);
-               
+    // Set the horizontal position in pixels of each dot on the progress bar
+    for (i = 0; i < this.props.stepCount; i++) {
+      this.progressBarDots = this.appendObjTo(this.progressBarDots, {
+        key: i,
+        left: 10 + i * stepWidth,
+      });
     }
 
-    _stepButtonClicked = (key) => {
-	
-	Animated.parallel(this.props.getStepAnimation(this.progressBarDots[key].key, this.props.currentStepAV <= key? true:false)).start();
+    // Contains the graphical components in the progress bar
+    this.progressBarComponents = [];
 
-	if(this.props.onStepChange != undefined)
-	    this.props.onStepChange(key);
+    /* Add the gray dots */
+    this.progressBarComponents = this.progressBarDots.map(item => (
+      <View style={[styles.dot, { left: item.left }, { top: this.props.animationHeight }]} key={item.key} />
+    ));
+
+    /* Add the gray bar */
+    this.progressBarComponents.push(
+      <Animated.View
+        style={[
+          styles.progress_bar,
+          { top: this.props.animationHeight + 6 },
+          { width: this.progressBarWidth },
+          { left: 20 },
+        ]}
+        key={3 * this.progressBarComponents.length + 1}
+      />,
+    );
+
+    /* Add the blue dots */
+    this.progressBarComponents = this.progressBarComponents.concat(
+      this.progressBarDots.map(item => (
+        <Animated.View
+          style={[
+            styles.activated_dot,
+            { opacity: this.dotsOpacity[item.key] },
+            { left: this.progressBarDots[item.key].left },
+            { top: this.props.animationHeight },
+          ]}
+          key={this.progressBarDots[item.key].key + this.progressBarDots.length}
+        />
+      )),
+    );
+
+    /* Add the objects used to detect the clicks on the dots */
+    //        if(this.props.animationWidth > 50)
+    this.progressBarComponents = this.progressBarComponents.concat(
+      this.progressBarDots.map(item => (
+        <TouchableOpacity
+          style={[
+            { position: 'absolute' },
+            { height: 40 },
+            { width: 40 },
+            { left: this.progressBarDots[item.key].left - 15 },
+            { top: this.props.animationHeight - 15 },
+          ]}
+          onPress={() => this._stepButtonClicked(item.key)}
+          key={1000 + item.key}
+        />
+      )),
+    );
+
+    /* Add the step numbers */
+    this.progressBarComponents = this.progressBarComponents.concat(
+      this.progressBarDots.map(item => (
+        <Text
+          style={[{ position: 'absolute' }, { left: item.left + 4 }, { top: this.props.animationHeight - 19 }]}
+          key={item.key + 2 * this.progressBarDots.length}
+        >
+          {item.key + 1}
+        </Text>
+      )),
+    );
+
+    /* Add the blue bar */
+    this.progressBarComponents.push(
+      <Animated.View
+        style={[
+          styles.activated_progress_bar,
+          { top: this.props.animationHeight + 6 },
+          { width: this.dynamiqueCurrentStep },
+          { left: 20 },
+        ]}
+        key={3 * this.progressBarComponents.length}
+      />,
+    );
+  }
+
+  _stepButtonClicked = key => {
+    Animated.parallel(
+      this.props.getStepAnimation(this.progressBarDots[key].key, this.props.currentStepAV <= key),
+    ).start();
+
+    if (this.props.onStepChange != undefined) this.props.onStepChange(key);
+  };
+
+  appendObjTo(thatArray, newObj) {
+    const frozenObj = Object.freeze(newObj);
+    return Object.freeze(thatArray.concat(frozenObj));
+  }
+
+  /** Get an array of Animated.timing which update the opacity of the active dots according to a step.
+   * The opacity of the dots of id <= stepId are set to 1
+   * The opacity of the other dots is set to 0
+   */
+
+  getOpacityAnimation(stepId) {
+    var stepAnimation = [];
+
+    /* Color the dots of id <= stepId */
+    for (var i = 1; i <= stepId; i++) {
+      stepAnimation.push(
+        Animated.timing(this.dotsOpacity[i], {
+          toValue: 1,
+          duration: this.props.stepLength,
+          easing: Easing.cubic,
+          key: i,
+        }),
+      );
     }
 
-    appendObjTo(thatArray, newObj) {
-	const frozenObj = Object.freeze(newObj);
-	return Object.freeze(thatArray.concat(frozenObj));
+    /* Hide the dots of id > stepId */
+    for (i = stepId + 1; i < this.props.stepCount; i++) {
+      stepAnimation.push(
+        Animated.timing(this.dotsOpacity[i], {
+          toValue: 0,
+          duration: this.props.stepLength,
+          easing: Easing.cubic,
+          key: i,
+        }),
+      );
     }
 
-    /** Get an array of Animated.timing which update the opacity of the active dots according to a step.
-     * The opacity of the dots of id <= stepId are set to 1
-     * The opacity of the other dots is set to 0
-     */ 
-    getOpacityAnimation(stepId){
-	
-        var stepAnimation = [];
+    return stepAnimation;
+  }
 
-	/* Color the dots of id <= stepId */
-	for(var i = 1; i <= stepId; i++){ 
-            stepAnimation.push(Animated.timing(
-		this.dotsOpacity[i],
-		{
-                    toValue: 1,
-                    duration: this.props.stepLength,
-                    easing: Easing.cubic,
-		    key: i
-		}
-            ));
-	}
-	
-	/* Hide the dots of id > stepId */
-	for(i = stepId + 1; i < this.props.stepCount; i++){ 
-            stepAnimation.push(Animated.timing(
-		this.dotsOpacity[i],
-		{
-                    toValue: 0,
-                    duration: this.props.stepLength,
-                    easing: Easing.cubic,
-		    key: i
-		}
-            ));
-	}
-
-        return stepAnimation;
-        
-    }
-
-    render(){
-        return((this.progressBarComponents));
-    }
-
+  render() {
+    return this.progressBarComponents;
+  }
 }
-                                 
+
 export default ProgressBar;
 
-
 const styles = StyleSheet.create({
-
-    activated_dot: {
-        position: 'absolute',
-	height: 15,
-	width: 15,
-        borderRadius: 10,
-        borderWidth: 0,
-	backgroundColor: 'blue'
-    },
-    dot: {
-        position: 'absolute',
-	height: 15,
-	width: 15,
-        borderRadius: 10,
-        borderWidth: 0,
-	backgroundColor: 'gray'
-    },
-    activated_progress_bar: {
-	position: 'absolute',
-	backgroundColor: 'blue',
-	height: 3
-    },
-    progress_bar: {
-	position: 'absolute',
-	backgroundColor: 'gray',
-	height: 3
-    },
-    
+  activated_dot: {
+    position: 'absolute',
+    height: 15,
+    width: 15,
+    borderRadius: 10,
+    borderWidth: 0,
+    backgroundColor: 'blue',
+  },
+  dot: {
+    position: 'absolute',
+    height: 15,
+    width: 15,
+    borderRadius: 10,
+    borderWidth: 0,
+    backgroundColor: 'gray',
+  },
+  activated_progress_bar: {
+    position: 'absolute',
+    backgroundColor: 'blue',
+    height: 3,
+  },
+  progress_bar: {
+    position: 'absolute',
+    backgroundColor: 'gray',
+    height: 3,
+  },
 });

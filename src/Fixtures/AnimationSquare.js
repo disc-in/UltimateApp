9ec2cfa22a178f90f47546bcc98bb645632987a2
Romain <aenario@gmail.com>
2020-04-29 16:36:51 +0200

    Update fixtures to match new animation format specification

--- src/Fixtures/AnimationSquare.js
+++ src/Fixtures/AnimationSquare.js
@@ -2,11 +2,10 @@
 // - positions[stepId][elemId][subStepId][coordinateId]
 //
 // Remark1: positions[0][elemId] is the initial position which must be defined for each element
-// Remark2: positions[stepId][elemId] is undefined if element i position does not change between steps stepId-1 and stepId
-// Remark3: the coordinates are expressed in percentage in [0, 1] of the space available for the animation
+// Remark2: positions[stepId][elemId] is undefined if  element i position does not change between steps stepId-1 and stepId
 
 /* Type of the element displayed */
-const ids = [
+var ids = [
   'triangle',
   'triangle',
   'triangle',
@@ -24,19 +23,25 @@ const ids = [
 ];
 
 /* Text displayed on each element */
-const texts = ['', '', '', '', '', '', '', '', '1', '2', '3', '4', '5', ''];
+var texts = ['', '', '', '', '', '', '', '', '1', '2', '3', '4', '5', ''];
 
 /* Definition of the position of each element at each step */
-const stepCount = 6;
+var stepCount = 6;
 
 var discDeltaP = 0.06;
 var discDeltaN = -0.02;
 
-const positions = new Array(stepCount);
+var positions = new Array(stepCount);
 
 // Initial positions
 positions[0] = new Array(texts.length);
 
+positions[0][13] = [[0.3 + discDeltaP, 0.3 + discDeltaP]];
+positions[0][8] = [[0.3, 0.3]];
+positions[0][9] = [[0.16, 0.3]];
+positions[0][10] = [[0.6, 0.3]];
+positions[0][11] = [[0.6, 0.6]];
+positions[0][12] = [[0.3, 0.6]];
 positions[0][0] = [[0.3, 0.3]];
 positions[0][1] = [[0.3, 0.6]];
 positions[0][2] = [[0.6, 0.3]];
@@ -45,12 +50,6 @@ positions[0][4] = [[0.45, 0.2]];
 positions[0][5] = [[0.2, 0.45]];
 positions[0][6] = [[0.45, 0.7]];
 positions[0][7] = [[0.7, 0.45]];
-positions[0][8] = [[0.3, 0.3]];
-positions[0][9] = [[0.16, 0.3]];
-positions[0][10] = [[0.6, 0.3]];
-positions[0][11] = [[0.6, 0.6]];
-positions[0][12] = [[0.3, 0.6]];
-positions[0][13] = [[0.3 + discDeltaP, 0.3 + discDeltaP]];
 
 // Step 1 - p2 first cut
 positions[1] = new Array(texts.length);
@@ -80,4 +79,4 @@ positions[5][12] = [[0.3, 0.3]];
 positions[5][8] = [[0.45, 0.2]];
 positions[5][13] = [[0.3 + discDeltaP, 0.3 + discDeltaP]];
 
-export default { positions, ids, texts };
+export default { ids, texts, positions };

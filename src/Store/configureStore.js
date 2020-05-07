53861d6e0764dad231bbde9ab04c14fc50fdec84
Adelie Busson <adelie@padam.io>
2020-05-07 15:36:18 +0200

    persist complete trainings and favorites

--- src/Store/configureStore.js
+++ src/Store/configureStore.js
@@ -1,9 +1,29 @@
-import { createStore } from 'redux';
+import { AsyncStorage } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { createLogger } from 'redux-logger';
+
 import rootReducer from './Reducers';
 import fixtures from '../Fixtures';
 
-export default createStore(
-  rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['completeTrainings', 'favoriteDrills'],
+  blacklist: ['drills', 'trainings', 'programs'],
+};
+// Middleware: Redux Persist Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Redux: Store
+const store = createStore(
+  persistedReducer,
   fixtures,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(createLogger()),
 );
+// Middleware: Redux Persist Persister
+const persistor = persistStore(store);
+
+// Exports
+export { store, persistor };

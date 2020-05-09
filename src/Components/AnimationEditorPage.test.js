14eb75b431258c09e0dca2ed91a184e40752b1ea
Martin Salles <salles.martin@gmail.com>
2020-05-09 16:48:15 +0200

    Fix test

--- src/Components/AnimationEditorPage.test.js
+++ src/Components/AnimationEditorPage.test.js
@@ -3,6 +3,8 @@ import renderer from 'react-test-renderer';
 
 import AnimationEditorPage from './AnimationEditorPage';
 
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
+
 describe('<AnimationEditorPage />', () => {
   it('renders correctly', () => {
     const navigation = { navigate: jest.fn(), setOptions: jest.fn() };

346d3cbc1cd3f88b0351b1d12b9571f8511ea665
Martin Salles <salles.martin@gmail.com>
2020-05-21 23:36:24 +0200

    Set default locale

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -4,6 +4,7 @@ import * as Localization from 'expo-localization';
 import en from './locales/en';
 import fr from './locales/fr';
 
+I18n.defaultLocale = 'en';
 I18n.locale = Localization.locale;
 I18n.fallbacks = true;
 I18n.translations = {

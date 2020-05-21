9c6f6b9b31d436174a82c63e5ae52a7af846c320
Martin Salles <salles.martin@gmail.com>
2020-05-21 18:55:49 +0200

    L12n and I18n (#148)
    * Add basic translations
    
    * Tidy up locales files

---
+++ src/utils/locales/fr.js
@@ -0,0 +1,8 @@
+export default {
+  homePage: {
+    frisbeeDrills: 'Exercices de Frisbee',
+    fitnessDrills: 'Exercices de Fitness',
+    programs: "Programmes d'entraînement",
+    editor: 'Editeur de drills',
+  },
+};

0d20511b5e12650c57ed1caa9b9856e28b4491b1
guillaumemoby <guillaume.espitia@gmail.com>
2020-05-31 15:49:48 +0200

    removing the tab label

--- src/Components/TheoryPage.js
+++ src/Components/TheoryPage.js
@@ -783,6 +783,7 @@ export default function TheoryPage() {
       activeColor={theme.COLOR_PRIMARY}
       inactiveColor={theme.COLOR_SECONDARY}
       barStyle={{ backgroundColor: theme.BACKGROUND_COLOR }}
+      shifting
     >
       <Tab.Screen
         name="Dictionary"

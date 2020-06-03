8aaa3a8a557ac5f1fb9f3bc06c5f638c62c9a03b
Martin Salles <salles.martin@gmail.com>
2020-06-03 11:22:40 +0200

    Simplify program layout

--- src/Components/programs/Program.js
+++ src/Components/programs/Program.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import ListItem from '../shared/ListItem';
 
 import I18n from '../../utils/i18n';
 import theme from '../../styles/theme.style';
+import ListItem from '../shared/ListItem';
 
 export const Program = props => {
   const { title, trainings } = props.program;
@@ -17,54 +17,32 @@ export const Program = props => {
   return (
     <ListItem>
       <View style={styles.program}>
-        <View style={styles.image}>
-          <View style={styles.wrapper}>
-            <Text style={styles.programTitle}>{title}</Text>
-          </View>
-          <View style={styles.presentationContainer}>
-            <Text style={styles.completion}>
-              {I18n.t('programs.program.completion', { done: completeTrainingsCount, total: trainings.length })}
-            </Text>
-            <View style={styles.progressBar}>
-              <View style={[StyleSheet.absoluteFill, styles.fillProgressBar, { width }]} />
-            </View>
-          </View>
+        <Text style={styles.programTitle}>{title}</Text>
+        <Text style={styles.completion}>
+          {I18n.t('programs.program.completion', { done: completeTrainingsCount, total: trainings.length })}
+          {completeTrainingsCount === trainings.length && ' 👍🎉'}
+        </Text>
+        <View style={styles.progressBar}>
+          <View style={[StyleSheet.absoluteFill, styles.fillProgressBar, { width }]} />
         </View>
       </View>
     </ListItem>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    completeTrainings: state.completeTrainings,
-  };
-};
+const mapStateToProps = state => ({
+  completeTrainings: state.completeTrainings,
+});
 
 export default connect(mapStateToProps)(Program);
 
 const styles = StyleSheet.create({
   program: {
-    paddingTop: 3,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  presentationContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    paddingVertical: 30,
+    paddingVertical: 10,
     paddingHorizontal: 20,
-  },
-  image: {
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: theme.COLOR_PRIMARY_LIGHT,
-    height: 60,
     width: '100%',
-    justifyContent: 'space-evenly',
   },
   programTitle: {
     fontSize: theme.FONT_SIZE_LARGE,
@@ -73,6 +51,7 @@ const styles = StyleSheet.create({
   },
   completion: {
     textAlign: 'right',
+    width: '100%',
     color: theme.COLOR_PRIMARY,
     fontSize: theme.FONT_SIZE_SMALL,
   },
@@ -87,26 +66,5 @@ const styles = StyleSheet.create({
     width: '50%',
     borderRadius: 5,
     backgroundColor: theme.GRADIENT_FIRST_COLOR,
-  },
-  ctaContainer: {
-    flexBasis: '10%',
-    paddingLeft: 20,
-  },
-  cta: {
-    height: 20,
-    aspectRatio: 109 / 239,
-  },
-  imageOpacity: {
-    opacity: 0.4,
-  },
-  wrapper: {
-    position: 'absolute',
-    top: -10,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    padding: 20,
   },
 });

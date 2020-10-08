0719ab8c6bf752a8d6866af07ddec3c5cd762b8c
Martin Salles <salles.martin@gmail.com>
2020-10-08 17:50:25 +0200

    Use Provider in App

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -19,86 +19,75 @@ import VideoPage from '../Components/VideoPage';
 
 import I18n from '../utils/i18n';
 import { DrillTypes, EquipmentLabels } from '../Fixtures/config';
-import { Provider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
 export const Navigation = () => (
-  <Provider>
-    <Stack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
-      <Stack.Screen name="HomePage" component={HomePage} options={{ title: I18n.t('navigation.homePage') }} />
-      <Stack.Screen
-        name="DrillListPage"
-        component={DrillListPage}
-        options={({ route }) => ({
-          title: I18n.t('navigation.drillListPage', {
-            type: route.params.type.substr(0, 1).toUpperCase() + route.params.type.substr(1),
-          }),
-        })}
-      />
-      <Stack.Screen
-        name="FrisbeeFilters"
-        component={FrisbeeFilters}
-        options={{ title: I18n.t('navigation.filters') }}
-      />
-      <Stack.Screen
-        name="FitnessFilters"
-        component={FitnessFilters}
-        options={{ title: I18n.t('navigation.filters') }}
-      />
-      <Stack.Screen
-        name="DrillPage"
-        component={DrillPage}
-        options={({ route }) => ({ title: route.params.drill.title })}
-      />
-      <Stack.Screen
-        name="ProgramListPage"
-        component={ProgramListPage}
-        options={({ route }) => {
-          if (route.params.type === DrillTypes.FRISBEE) return { title: I18n.t('navigation.programListPage.frisbee') };
-          return {
-            title:
-              route.params.equipmentLabel === EquipmentLabels.NONE
-                ? I18n.t('navigation.programListPage.noEquipment')
-                : I18n.t('navigation.programListPage.fullEquipment'),
-          };
-        }}
-      />
-      <Stack.Screen
-        name="TrainingPage"
-        component={TrainingPage}
-        options={({ route }) => ({
-          title: route.params.program?.title || route.params.training.title,
-        })}
-      />
-      <Stack.Screen
-        name="DrillPageMinimal"
-        component={DrillPageMinimal}
-        options={({ route }) => ({
-          title: I18n.t('navigation.drillPageMinimal', { training: route.params.training.title }),
-        })}
-      />
-      <Stack.Screen
-        name="AnimationEditorPage"
-        component={AnimationEditorPage}
-        options={() => ({ title: I18n.t('navigation.animationEditorPage') })}
-      />
-      <Stack.Screen
-        name="DictionaryPage"
-        component={DictionaryPage}
-        options={{ title: I18n.t('navigation.dictionaryPage') }}
-      />
-      <Stack.Screen
-        name="EssentialPage"
-        component={EssentialPage}
-        options={{ title: I18n.t('navigation.essentialPage') }}
-      />
-      <Stack.Screen name="TacticsPage" component={TacticsPage} options={{ title: I18n.t('navigation.tacticsPage') }} />
-      <Stack.Screen
-        name="VideoPage"
-        component={VideoPage}
-        options={({ route }) => ({ title: route.params.video.title })}
-      />
-    </Stack.Navigator>
-  </Provider>
+  <Stack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
+    <Stack.Screen name="HomePage" component={HomePage} options={{ title: I18n.t('navigation.homePage') }} />
+    <Stack.Screen
+      name="DrillListPage"
+      component={DrillListPage}
+      options={({ route }) => ({
+        title: I18n.t('navigation.drillListPage', {
+          type: route.params.type.substr(0, 1).toUpperCase() + route.params.type.substr(1),
+        }),
+      })}
+    />
+    <Stack.Screen name="FrisbeeFilters" component={FrisbeeFilters} options={{ title: I18n.t('navigation.filters') }} />
+    <Stack.Screen name="FitnessFilters" component={FitnessFilters} options={{ title: I18n.t('navigation.filters') }} />
+    <Stack.Screen
+      name="DrillPage"
+      component={DrillPage}
+      options={({ route }) => ({ title: route.params.drill.title })}
+    />
+    <Stack.Screen
+      name="ProgramListPage"
+      component={ProgramListPage}
+      options={({ route }) => {
+        if (route.params.type === DrillTypes.FRISBEE) return { title: I18n.t('navigation.programListPage.frisbee') };
+        return {
+          title:
+            route.params.equipmentLabel === EquipmentLabels.NONE
+              ? I18n.t('navigation.programListPage.noEquipment')
+              : I18n.t('navigation.programListPage.fullEquipment'),
+        };
+      }}
+    />
+    <Stack.Screen
+      name="TrainingPage"
+      component={TrainingPage}
+      options={({ route }) => ({
+        title: route.params.program?.title || route.params.training.title,
+      })}
+    />
+    <Stack.Screen
+      name="DrillPageMinimal"
+      component={DrillPageMinimal}
+      options={({ route }) => ({
+        title: I18n.t('navigation.drillPageMinimal', { training: route.params.training.title }),
+      })}
+    />
+    <Stack.Screen
+      name="AnimationEditorPage"
+      component={AnimationEditorPage}
+      options={() => ({ title: I18n.t('navigation.animationEditorPage') })}
+    />
+    <Stack.Screen
+      name="DictionaryPage"
+      component={DictionaryPage}
+      options={{ title: I18n.t('navigation.dictionaryPage') }}
+    />
+    <Stack.Screen
+      name="EssentialPage"
+      component={EssentialPage}
+      options={{ title: I18n.t('navigation.essentialPage') }}
+    />
+    <Stack.Screen name="TacticsPage" component={TacticsPage} options={{ title: I18n.t('navigation.tacticsPage') }} />
+    <Stack.Screen
+      name="VideoPage"
+      component={VideoPage}
+      options={({ route }) => ({ title: route.params.video.title })}
+    />
+  </Stack.Navigator>
 );

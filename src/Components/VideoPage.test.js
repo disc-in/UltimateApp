57dd83beeb9b2b4dca069ec39f744eac621aab89
Martin Salles <salles.martin@gmail.com>
2020-11-10 20:04:24 +0100

    Fix test

--- src/Components/VideoPage.test.js
+++ src/Components/VideoPage.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { NavigationContext } from '@react-navigation/native';
 
 import VideoPage from './VideoPage';
 
@@ -10,8 +11,18 @@ describe('<VideoPage />', () => {
         video: 1,
       },
     };
+    // fake NavigationContext value data
+    const navContext = {
+      isFocused: () => true,
+      // addListener returns an unscubscribe function.
+      addListener: jest.fn(() => jest.fn()),
+    };
 
-    const { toJSON } = render(<VideoPage route={route} />);
+    const { toJSON } = render(
+      <NavigationContext.Provider value={navContext}>
+        <VideoPage route={route} />
+      </NavigationContext.Provider>,
+    );
     expect(toJSON()).toMatchSnapshot();
   });
 });

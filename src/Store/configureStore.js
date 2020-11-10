d40a91f7422a2dee55c98e487fe1cd51c7e5878b
Martin Salles <salles.martin@gmail.com>
2020-11-11 00:22:21 +0100

    Add store migration

--- src/Store/configureStore.js
+++ src/Store/configureStore.js
@@ -1,15 +1,34 @@
 import { AsyncStorage } from 'react-native';
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 
 import rootReducer from './Reducers';
 import fixtures from '../Fixtures';
 
+const migrations = {
+  1: (state) => {
+    // migration to rename customDrills to customPlays modifying the structure
+    const customPlays = state.customDrills?.map((drill) => {
+      return {
+        animation: drill.drill,
+        title: drill.title,
+      };
+    });
+    return {
+      ...state,
+      customPlays,
+      customDrills: undefined,
+    };
+  },
+};
+
 const persistConfig = {
   key: 'root',
+  version: 1,
   storage: AsyncStorage,
   whitelist: ['completeTrainings', 'favoriteDrills', 'customPlays'],
   blacklist: ['drills', 'trainings', 'programs', 'theory'],
+  migrate: createMigrate(migrations, { debug: false }),
 };
 // Middleware: Redux Persist Persisted Reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);

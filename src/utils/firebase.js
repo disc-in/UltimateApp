4199eaf402bc18730d748ab8a770ac34c3a7a0cd
Martin Salles <salles.martin@gmail.com>
2021-01-05 15:36:51 +0100

    Remove unused variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -49,11 +49,8 @@ if (Platform.OS === 'android') {
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.EXPO_FIREBASE_DATABASE_URL,
   projectId: process.env.EXPO_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_FIREBASE_APP_ID,
 };
 

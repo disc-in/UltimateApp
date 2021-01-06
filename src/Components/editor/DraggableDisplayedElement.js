fa2d2529972fceaa327b68bc367ed7950d7ab7fa
Martin Salles <salles.martin@gmail.com>
2021-01-06 18:01:40 +0100

    Remove useless import

--- src/Components/editor/DraggableDisplayedElement.js
+++ src/Components/editor/DraggableDisplayedElement.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Animated, PanResponder } from 'react-native';
 
 import theme from '../../styles/theme.style';

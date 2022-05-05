4c8af22b3bbddf4c07e2a0bd7f2b33801fa2bf50
Martin Salles <salles.martin@gmail.com>
2022-05-05 15:23:51 +0200

    Fix reanimated, splash-scren, async-storage

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['module:react-native-dotenv'],
+    plugins: ['module:react-native-dotenv', 'react-native-reanimated/plugin'],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
